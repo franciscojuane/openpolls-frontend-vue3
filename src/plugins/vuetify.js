3f4abf58cc4c78c51cb9e55bde0dfc378ec43a7e
Francisco Juane <franciscojuaneutn@gmail.com>
2025-03-19 17:29:49 -0300

    Vuetify configuration updated

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,9 +1,7 @@
-import Vue from "vue";
-import Vuetify from "vuetify";
+import { createVuetify } from "vuetify";
 import "vuetify/dist/vuetify.min.css";
-Vue.use(Vuetify);
 
-export default new Vuetify({
+export default new createVuetify({
   theme: {
     dark: false,
     options: {
