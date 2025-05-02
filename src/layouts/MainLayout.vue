1047896ef4f147bd305906a6c5ca5113031a9088
Francisco Juane <franciscojuaneutn@gmail.com>
2025-05-02 20:10:06 -0300

    MainLayout migration finished

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,17 +4,18 @@
       <v-app-bar app color="secondary" dark>
         <v-toolbar dense color="secondary" class="elevation-12"
           ><v-toolbar-title
-            @click="$router.push({ name: 'pollList' })"
+            @click="router.push({ name: 'pollList' })"
             style="cursor: pointer"
+            class="text-start"
             >OpenPolls</v-toolbar-title
           >
           <v-spacer></v-spacer>
           {{ currentUser ? currentUser.firstName : "" }}
           {{ currentUser ? currentUser.lastName : "" }}
-          <v-tooltip bottom>
-            <template v-slot:activator="{ on, attrs }">
+          <v-tooltip location="bottom">
+            <template v-slot:activator="{ props }">
               <v-btn icon>
-                <v-icon @click="logOut" v-on="on" v-bind="attrs"
+                <v-icon @click="() => logOut()" v-bind="props"
                   >mdi-account-arrow-right</v-icon
                 >
               </v-btn>
@@ -35,19 +36,20 @@
   </v-app>
 </template>
 
-<script>
-import { mapState } from "vuex";
+<script setup>
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+import { inject } from "vue";
 
-export default {
-  name: "MainLayout",
-  methods: {
-    logOut() {
-      this.$auth.logout();
-      this.$router.push({ name: "loginView" });
-    },
-  },
-  computed: {
-    ...mapState(["currentUser"]),
-  },
-};
+const router = useRouter();
+const auth = inject("auth");
+
+function logOut() {
+  auth.logout();
+  router.push({ name: "loginView" });
+}
+
+let store = useStore();
+
+let currentUser = store.getters.currentUser;
 </script>
